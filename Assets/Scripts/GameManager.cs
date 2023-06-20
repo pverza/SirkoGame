22d5d4af87d4a03a2f023394304d8ea129e5e43a
pverza <pverza@hotmail.it>
2023-06-20 22:09:06 +0200

    Multiplayer almost working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -45,7 +46,7 @@ public class GameManager : MonoBehaviour
             numberOfTurnsAvailableForCurrentPlayer = movesPerPlayer;
             Piece.resetAllPiecesStatus();
         }
-        if (!ignoreTurns) { uiText.text = "Turn: " + currentTeamThatIsPlayng.ToString() + "\nMoves:" + numberOfTurnsAvailableForCurrentPlayer; }
+        if (!ignoreTurns) { UpdateText("Turn: " + currentTeamThatIsPlayng.ToString() + "\nMoves:" + numberOfTurnsAvailableForCurrentPlayer); }
     }
 
     public bool IsTeamsTurn(PieceTeam pieceTeam) {
@@ -59,9 +60,123 @@ public class GameManager : MonoBehaviour
     }
 
     public void GameOver(bool isStall) {
-        uiText.text = currentTeamThatIsPlayng.ToString() + " Player \nWins";
+        UpdateText(currentTeamThatIsPlayng.ToString() + " Player \nWins");
         isGameover = true;
     }
 
+
     //todo: I need to check if all players are blocked
+    //very hacky way to to set the networkBehaviour.
+    public List<Piece> allPieces;
+
+    public Piece getPieceWithPieceID(int pieceId) {
+        foreach (Piece p in allPieces) {
+            if (p.pieceId == pieceId) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public Cell [] cells;
+
+    public int[] GetHiglightedStatus() {
+        List<int> highlightedCells = new List<int>();
+        for (int i = 0; i < cells.Length; i++)
+        {
+            if (cells[i].higlited)
+            {
+                highlightedCells.Add(i);
+            }
+        }
+        return highlightedCells.ToArray();
+    }
+
+    public int[] GetBlockedPositions() {
+        List<int> blockedPositions = new List<int>();
+        for (int i = 0; i < cells.Length; i++)
+        {
+            if (cells[i].blockedCellMesh.activeSelf)
+            {
+                blockedPositions.Add(i);
+            }
+        }
+        return blockedPositions.ToArray();
+    }
+
+    public int[] GetBlockedPositions1() {
+        List<int> blockedPositions = new List<int>();
+        for (int i = 0; i < cells.Length; i++)
+        {
+            if (cells[i].blockedCellMesh1.activeSelf)
+            {
+                blockedPositions.Add(i);
+            }
+        }
+        return blockedPositions.ToArray();
+    }
+
+    public void ResetBlocked() {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            cells[i].blockedCellMesh.SetActive(false);
+            cells[i].blockedCellMesh1.SetActive(false);
+        }
+    }
+
+    public void SetBlockedPositions(int[] positionsToBlock) {
+        for (int i = 0; i < positionsToBlock.Length; i++) {
+            cells[positionsToBlock[i]].blockedCellMesh.SetActive(true);
+        }
+    }
+
+    public void SetBlockedPositions1(int[] positionsToBlock)
+    {
+        for (int i = 0; i < positionsToBlock.Length; i++)
+        {
+            cells[positionsToBlock[i]].blockedCellMesh1.SetActive(true);
+        }
+    }
+
+    public void SetHiglightedCells(int[] positionsToHiglight)
+    {
+        ResetHighlightedCells();
+
+        for (int i = 0; i < positionsToHiglight.Length; i++)
+        {
+            Cell cell = cells[positionsToHiglight[i]];
+            Renderer renderer = cell.GetComponent<Renderer>();
+            // Check if the renderer component exists
+            if (renderer != null)
+            {
+                // Change the material
+                renderer.material = cell.highlitedMaterial;
+                cell.higlited = true;
+            }
+            
+        }
+    }
+
+    public void ResetHighlightedCells() {
+        foreach (Cell c in cells)
+        {
+            Renderer renderer = c.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material = c.defaultMaterial;
+            }
+        }
+    }
+    private void UpdateText(string text) {
+        uiText.text = text;
+        if (NetworkManager.Singleton.IsServer) {
+            UpdateTextRpc(text);
+        }
+    }
+
+    [ClientRpc]
+    private void UpdateTextRpc(string text)
+    {
+        uiText.text = text;
+    }
 }
--- Assets/Scripts/MouseBehaviour.cs
+++ Assets/Scripts/MouseBehaviour.cs
@@ -1,42 +1,174 @@
-﻿using UnityEngine;
+﻿using Unity.Netcode;
+using UnityEngine;
 
-public class MouseBehaviour : MonoBehaviour
+public class MouseBehaviour : NetworkBehaviour
 {
     private Piece grabbedPiece;
+    [SerializeField]
     private bool isGrabbed = false;
     public LayerMask LayerMask;
 
+    private Vector3 offset;
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (NetworkManager.Singleton.IsServer)
         {
-            if (!isGrabbed)
+            if (Input.GetMouseButtonDown(0))
             {
-                // Raycast to check if we hit a GameObject
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask))
-                {
-                    Piece piece = hit.collider.gameObject.GetComponent<Piece>();
-                    if (piece != null &&  GameManager.instance.IsTeamsTurn(piece.pieceTeam) && !piece.isAlreadyMoved && !GameManager.instance.isGameover)
-                    {
-                        grabbedPiece = piece;
-                        grabbedPiece.Clicked();
-                        isGrabbed = true;
-                    }
-                }
+                PerformMovementAndLogic();
             }
-            else
+            //movement a bith hacky but I need to see if it works
+            Move();
+        }
+        else 
+        {
+            if (Input.GetMouseButtonDown(0))
             {
-                // Release the grabbed GameObject
-                grabbedPiece.DeClicked();
-                grabbedPiece = null;
-                isGrabbed = false;
+                SendServerMessageServerRpc(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            }
+            if (isGrabbed) //for the movement
+            {
+                Vector3 targetPosition = GetMouseWorldPosition() + offset;
+                SendServerMessageForMovementServerRpc(targetPosition.x, targetPosition.z);
             }
         }
 
-
+            
     }
 
+    //private void NetworkUpdate()
+    //{
+    //    if (isGrabbed) //for the movement
+    //    {
+    //        Vector3 targetPosition = GetMouseWorldPosition() + offset;
+    //        SendServerMessageForMovementServerRpc(targetPosition.x, targetPosition.z);
+    //    }
+    //}
+
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            return ray.GetPoint(rayDistance);
+        }
+
+        return Vector3.zero;
+    }
+
+
+    private NetworkClient networkClient;
+
+    private void Start()
+    {
+        networkClient = NetworkManager.Singleton.LocalClient;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SendServerMessageServerRpc(float mousePositionX, float mousePositionY, float mousePositionZ)
+    {
+        if (!isGrabbed)
+        {
+            // Raycast to check if we hit a GameObject
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(mousePositionX, mousePositionY, mousePositionZ));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask))
+            {
+                Piece piece = hit.collider.gameObject.GetComponent<Piece>();
+                if (piece != null && GameManager.instance.IsTeamsTurn(piece.pieceTeam) && !piece.isAlreadyMoved && !GameManager.instance.isGameover)
+                {
+                    Piece grabbedPieceExtracted = GameManager.instance.getPieceWithPieceID(piece.pieceId);
+                    if (grabbedPieceExtracted == null) { return; }
+                    grabbedPiece = grabbedPieceExtracted;
+
+                    grabbedPiece.Clicked();
+                    isGrabbed = true;
+                    offset = grabbedPiece.transform.position - GetMouseWorldPosition(); // move thisone outside
+                }
+            }
+
+        }
+        else
+        {
+            // Release the grabbed GameObject
+            grabbedPiece.DeClicked();
+            grabbedPiece = null;
+            isGrabbed = false;
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SendServerMessageForMovementServerRpc(float xPosition, float zPosition)
+    {
+        Debug.Log(xPosition + " " + zPosition);
+        grabbedPiece.transform.position = new Vector3(xPosition, grabbedPiece.transform.position.y, zPosition);
+    }
+
+
+    void PerformMovementAndLogic() {
+        if (!isGrabbed)
+        {
+            // Raycast to check if we hit a GameObject
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask))
+            {
+                Piece piece = hit.collider.gameObject.GetComponent<Piece>();
+                if (piece != null && GameManager.instance.IsTeamsTurn(piece.pieceTeam) && !piece.isAlreadyMoved && !GameManager.instance.isGameover)
+                {
+                    grabbedPiece = piece;
+                    grabbedPiece.Clicked();
+                    isGrabbed = true;
+                    offset = grabbedPiece.transform.position - GetMouseWorldPosition();
+
+                    UpdateHiglightedClientRpc(GameManager.instance.GetHiglightedStatus());
+                }
+            }
+
+        }
+        else
+        {
+            // Release the grabbed GameObject
+            grabbedPiece.DeClicked();
+            grabbedPiece = null;
+            isGrabbed = false;
+
+            UpdateClientCellsAfterDeclickedClientRpc(GameManager.instance.GetBlockedPositions(), GameManager.instance.GetBlockedPositions1());
+        }
+    }
+
+    private void Move()
+    {
+        if (isGrabbed) //for the movement
+        {
+            Vector3 targetPosition = GetMouseWorldPosition() + offset;
+            grabbedPiece.transform.position = new Vector3(targetPosition.x, grabbedPiece.transform.position.y, targetPosition.z);
+        }
+    }
+
+    [ClientRpc]
+    private void UpdateHiglightedClientRpc(int [] cellsToUpdate)
+    {
+        //Debug.Log($"Received message from server: {message}");
+        // Process the received message on the client
+        GameManager.instance.SetHiglightedCells(cellsToUpdate);
+    }
+
+    [ClientRpc]
+    private void UpdateClientCellsAfterDeclickedClientRpc(int [] blockerStatus, int [] blocker1status)
+    {
+        //Debug.Log($"Received message from server: {message}");
+        // Process the received message on the client
+        GameManager.instance.ResetHighlightedCells();
+        GameManager.instance.ResetBlocked();
+        GameManager.instance.SetBlockedPositions(blockerStatus);
+        GameManager.instance.SetBlockedPositions1(blocker1status);
+    }
 }
